5403230632c71df6c3149ee865b091cf2eb7a4fc
Rooju Chokshi <roojuc@ntdev.microsoft.com>
2019-11-26 16:09:26 -0800

    Built with C++17; Remove Boost; Modify to output to a single header file

--- embedresource.cpp
+++ embedresource.cpp
@@ -1,50 +1,51 @@
-#include <boost/filesystem.hpp>
-#include <boost/filesystem/fstream.hpp>
+#include <fstream>
 #include <iostream>
-
-using namespace std;
-using namespace boost::filesystem;
+#include <filesystem>
 
 int main(int argc, char** argv)
 {
     if (argc < 3) {
         fprintf(stderr, "USAGE: %s {sym} {rsrc}\n\n"
-                        "  Creates {sym}.c from the contents of {rsrc}\n",
+                        "  Creates {sym}.h from the contents of {rsrc}\n",
                 argv[0]);
         return EXIT_FAILURE;
     }
 
-    path dst{argv[1]};
-    path src{argv[2]};
+    std::filesystem::path dst{argv[1]};
 
-    string sym = src.filename().string();
-    replace(sym.begin(), sym.end(), '.', '_');
-    replace(sym.begin(), sym.end(), '-', '_');
+    std::ofstream ofs{ dst };
+    ofs << "#pragma once" << std::endl;
+    ofs << "#include <stdlib.h>" << std::endl;
+    ofs << "#include <algorithm>" << std::endl;
 
-    create_directories(dst.parent_path());
-
-    boost::filesystem::ofstream ofs{dst};
-
-    boost::filesystem::ifstream ifs{src};
-
-    ofs << "#include <stdlib.h>" << endl;
-    ofs << "const char _resource_" << sym << "[] = {" << endl;
-
-    size_t lineCount = 0;
-    while (!ifs.eof())
+    for (auto i = 2; i < argc; i++)
     {
-        char c;
-        ifs.get(c);
-        ofs << "0x" << hex << (c&0xff) << ", ";
-        if (++lineCount == 10) {
-            ofs << endl;
-            lineCount = 0;
+        std::filesystem::path src{ argv[i] };
+
+        std::string sym = src.filename().string();
+        replace(sym.begin(), sym.end(), '.', '_');
+        replace(sym.begin(), sym.end(), '-', '_');
+
+        create_directories(dst.parent_path());
+
+        std::ifstream ifs{ src };
+
+        ofs << "const char " << sym << "[] = {" << std::endl;
+
+        size_t lineCount = 0;
+        while (ifs.good() && !ifs.eof())
+        {
+            char c;
+            ifs.get(c);
+            ofs << "0x" << std::hex << (c & 0xff) << ", ";
+            if (++lineCount == 10) {
+                ofs << std::endl;
+                lineCount = 0;
+            }
         }
+
+        ofs << "};" << std::endl << std::endl;
     }
-
-
-    ofs << "};" << endl;
-    ofs << "const size_t _resource_" << sym << "_len = sizeof(_resource_" << sym << ");";
 
     return EXIT_SUCCESS;
 }
